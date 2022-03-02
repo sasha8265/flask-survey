4d8851cdbed32a29f98c0548c4fc41f1db30360f
Sasha Czerniawski <sashacandoit@gmail.com>
2022-03-02 07:29:11 -0500

    session info added and some style adjustments

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from operator import methodcaller
-from flask import Flask, request, render_template, redirect, flash, jsonify, session
-from random import randint, choice, sample
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -11,8 +9,10 @@ app.config['SECRET_KEY'] = "secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-# Ask why we define responses as a string here
+# *********************************************
+# Ask why we define responses this way?
 RESPONSES = "responses"
+# *********************************************
 
 @app.route('/')
 def home_page():
@@ -21,27 +21,39 @@ def home_page():
 
 @app.route('/start', methods=["POST"])
 def start_survey():
+    """Starts a session when user starts survey and redirects to first question page"""
     session[RESPONSES] = []
-    return redirect("/questions/0")
+    username = request.args["name"]
+    return redirect("/questions/0", name=username)
 
 
 @app.route('/questions/<int:qid>')
-# how am i getting qid?
 def show_questions(qid):
     """Shows the next question and choices in sequence"""
+    
+    # gets the responses already submitted by the user in current session
     responses = session.get(RESPONSES)
+
+    print('****************************************')
+    print(session[RESPONSES])
+    print('****************************************')
     
     if (responses == None):
-        """redirect user accessing question page too soon"""
+        # redirect user accessing question page without starting session
         return redirect('/')
 
     if (len(responses) == len(survey.questions)):
+        # user completed all questions - show thank you page
         return redirect('/complete')
 
     if (len(responses) != qid):
+        # user is trying to access a question out of order
+        # number of responses does not match number in question sequence
+        
         flash("Please answer all questions in order")
+        
+        # returns user to the question matching their number of responses
         return redirect(f"/questions/{len(responses)}")
-
 
     question = survey.questions[qid]
     return render_template('questions.html', qid=qid, question=question)
@@ -49,7 +61,7 @@ def show_questions(qid):
 
 @app.route('/answer', methods=['POST'])
 def handle_response():
-    """Save response to question and redirect to next question"""
+    """Save response to session and redirects to next question"""
 
     # Get 'answer' from input name defined in questions.html
     choice = request.form['answer']
@@ -65,5 +77,5 @@ def handle_response():
 
 @app.route('/complete')
 def complete():
-    """User completed all questions - show thank you page"""
+    """User completed all questions - shows thank you page"""
     return render_template('complete.html')
